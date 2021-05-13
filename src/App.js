703d297f0a9ba5641bb3fe7b03884d14bf7c9e3c
pranayvarshney <pranay.varshney9@gmail.com>
2021-05-13 23:27:21 +0530

    added search bar, initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Searchbox from './components/Searchbox';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        {/* <Searchbox/> */}
+        <Sidebar />
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
