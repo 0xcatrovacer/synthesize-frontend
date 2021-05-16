1bebed3cf062f524ac53a834a93a2b5034c9f1da
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-16 21:02:49 +0530

    added Home component and configured routes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,35 +13,35 @@ const Sidebar = () => {
   };
   window.addEventListener("resize", showMobile);
   return (
-    
-      <div className={mobile ? "sidebar-mobile" : "sidebar-web"}>
-        <div className="branding">
-          <div className="logo">
-            <img src="logo.png" height="80px" width="80px" alt="" />
-          </div>
-          <p className="brand-name">Synthesize</p>
+
+    <div className={mobile ? "sidebar-mobile" : "sidebar-web"}>
+      <div className="branding">
+        <div className="logo">
+          <img src="logo.png" height="80px" width="80px" alt="" />
         </div>
-       
-        <ul className="container sidebar-mainlist">
-          <li className="sidebar-list-items">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="sidebar-list-items">
-            <Link to="/">Discover</Link>
-          </li>
-          
-          <li className="sidebar-list-items">
-            <Link to="/">Playlists</Link>
-          </li>
-          <li className="sidebar-list-items">
-            <Link to="/">New Played</Link>
-          </li>
-          <li className="sidebar-list-items">
-            <Link to="/">Liked songs</Link>
-          </li>
-        </ul>
+        <p className="brand-name">Synthesize</p>
       </div>
-    
+
+      <ul className="container sidebar-mainlist">
+        <li className="sidebar-list-items">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="sidebar-list-items">
+          <Link to="/">Discover</Link>
+        </li>
+
+        <li className="sidebar-list-items">
+          <Link to="/">Playlists</Link>
+        </li>
+        <li className="sidebar-list-items">
+          <Link to="/">New Played</Link>
+        </li>
+        <li className="sidebar-list-items">
+          <Link to="/">Liked songs</Link>
+        </li>
+      </ul>
+    </div>
+
   );
 };
 
