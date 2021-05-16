66ce8a80147cf1ed8734057e53b7c91ec5d15c6e
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-16 21:40:58 +0530

    handled user auth signout

--- src/components/Rightpane.js
+++ src/components/Rightpane.js
@@ -1,12 +1,33 @@
- import './Rightpane.css'
- 
- const Userdetails = () => {
-     return (<div className="right-pane">
-         <div className="user-details">
-             <div className="username">USERNAME</div>
-            <div className="setting-button"></div>
+import { GoogleLogout } from 'react-google-login'
+import { useHistory } from 'react-router'
+import './Rightpane.css'
+
+const Userdetails = () => {
+
+    const history = useHistory()
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token')
+        history.push('/')
+    }
+
+    return (<div className="right-pane">
+        <div className="user-details">
+            <div className="username">USERNAME</div>
+            <div className="setting-button">
+                <GoogleLogout
+                    clientId={process.env.REACT_APP_CLIENT_ID}
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign Out UwU. Iska css part jo tha usko comment out kar diya hai</button>
+                    )}
+                    buttonText="Logout"
+                    onLogoutSuccess={handleSignOut}
+                >
+                </GoogleLogout>
+            </div>
         </div>
-         <div className="recently-played"></div>
-     </div> )}
-  
- export default Userdetails;
+        <div className="recently-played"></div>
+    </div>)
+}
+
+export default Userdetails;
