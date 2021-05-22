2e9eae81e1c15be47d4cddd00b16bef9d6479fc3
Swarnab Garang <swarnabgarang@gmail.com>
2021-05-22 21:44:41 +0530

    handled and designed Search

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,31 +1,75 @@
 import './Searchbox.css'
 import React, { useState } from "react";
+import axios from 'axios';
 
 const Searchbox = () => {
-     const [mobile, setMobile] = useState(false);
-     const showMobile = () => {
-       if (window.innerWidth <= 800) {
-         setMobile(false);
-       } else {
-         setMobile(false);
-       }
-     };
-     window.addEventListener("resize", showMobile);
-    return (
-     
-        <div className={mobile ? "searchbox-mobile" : "searchbox-web"}>
-          <div className="search-window">
-            
-              <div className="search-icon">
-                <i className="fas fa-search fa-lg" id="icon"></i>
-              </div>
 
-              <input className="search-area" type="text"  />
-            
+  const [input, setInput] = useState('')
+
+  const [result, setResult] = useState([])
+
+  const [toggledrop, setToggledrop] = useState(false)
+
+  const handleSearch = () => {
+
+    const searchInput = { searchInput: input }
+
+    axios({
+      method: 'POST',
+      url: 'http://143.110.255.209:8000/search',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: searchInput
+    }).then((res) => {
+      setResult(res.data)
+    }).catch((e) => {
+      console.log({ error: e })
+    })
+  }
+
+
+  return (
+    <div className="search-wrapper">
+      <div className="searchbox-web">
+        <div className="search-window">
+
+          <div className="search-icon">
+            <i className="fas fa-search fa-lg" id="icon"></i>
+          </div>
+
+          <input className="search-area" type="text" value={input}
+            onChange={(e) => {
+              setInput(e.target.value)
+              handleSearch()
+            }}
+            onFocus={() => {
+              setToggledrop(true)
+            }}
+            onBlur={() => {
+              setToggledrop(false)
+            }}
+          />
+
+        </div>
+      </div>
+      {result && toggledrop && <div className='search-results'> {result.map((track) => (
+        <div className='each-search-result' key={track._id}>
+          <div>
+            <img src={track.imageURL} className="search-result-image" />
+          </div>
+          <div className="search-result-title">
+            {track.title}
+          </div>
+          <div className="search-result-artist">
+            {track.artists[0]}
           </div>
         </div>
-      
-    );
+      ))}
+      </div>}
+    </div>
+
+  );
 }
- 
+
 export default Searchbox;
--- src/components/SettingsMenu.js
+++ src/components/SettingsMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -6,64 +6,64 @@ import { GoogleLogout } from "react-google-login";
 import { useHistory } from "react-router";
 import SettingsIcon from "@material-ui/icons/Settings";
 import "./Rightpane.css"
-import {Link} from 'react-router-dom'
- const SettingsMenu = () => {
-     const [anchorEl, setAnchorEl] = React.useState(null);
-     const history = useHistory();
-     const handleSignOut = () => {
-       localStorage.removeItem("token");
-       history.push("/");
-     };
-     
-     const handleClick = (event) => {
-       setAnchorEl(event.currentTarget);
-     };
+import { Link } from 'react-router-dom'
+const SettingsMenu = () => {
+  const [anchorEl, setAnchorEl] = useState();
+  const history = useHistory();
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    history.push("/");
+  };
 
-     const handleClose = () => {
-       setAnchorEl(null);
-     };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-     return (
-       <div className="settingsButton" style={{ color: "white" }}>
-         <SettingsIcon
-           aria-controls="settings-menu"
-           aria-haspopup="true"
-           onClick={handleClick}
-           fontSize="inherit"
-         >
-           Open Menu
-         </SettingsIcon>
-         <Menu
-           id="settings-menu"
-           anchorEl={anchorEl}
-           keepMounted
-           open={Boolean(anchorEl)}
-           onClose={handleClose}
-         >
-           <MenuItem onClick={handleClose}>
-             {" "}
-             <Link id="about" to="/aboutus">
-               <Button> About Us</Button>
-             </Link>
-           </MenuItem>
-           <MenuItem onClick={handleClose}>
-             <GoogleLogout
-               clientId={process.env.REACT_APP_CLIENT_ID}
-               render={(renderProps) => (
-                 <Button
-                   onClick={renderProps.onClick}
-                   disabled={renderProps.disabled}
-                 >
-                   Log Out
-                 </Button>
-               )}
-               buttonText="Logout"
-               onLogoutSuccess={handleSignOut}
-             ></GoogleLogout>
-           </MenuItem>
-         </Menu>
-       </div>
-     );
- }
-  
- export default SettingsMenu;
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="settingsButton" style={{ color: "white" }}>
+      <SettingsIcon
+        aria-controls="settings-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        fontSize="inherit"
+      />
+      <Menu
+        id="settings-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link id="about" to="/aboutus" style={{
+            textDecoration: 'none',
+            color: 'black'
+          }}>
+            <div> About Us</div>
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <GoogleLogout
+            clientId={process.env.REACT_APP_CLIENT_ID}
+            render={(renderProps) => (
+              <div
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Log Out
+              </div>
+            )}
+            buttonText="Logout"
+            onLogoutSuccess={handleSignOut}
+          />
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default SettingsMenu;
