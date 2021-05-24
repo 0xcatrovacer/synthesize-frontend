31b9faecc61082efab0ab6549b4a757c1ee72402
pranayvarshney <pranay.varshney9@gmail.com>
2021-05-25 00:18:26 +0530

    added search bar functionality

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,75 +1,81 @@
-import './Searchbox.css'
+import "./Searchbox.css";
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
+import Playbar from './Playbar'
 
-const Searchbox = () => {
+const Searchbox = ({setPlayingTrack}) => {
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState('')
+  const [result, setResult] = useState([]);
 
-  const [result, setResult] = useState([])
-
-  const [toggledrop, setToggledrop] = useState(false)
+  const [toggledrop, setToggledrop] = useState(false);
 
   const handleSearch = () => {
-
-    const searchInput = { searchInput: input }
+    const searchInput = { searchInput: input };
 
     axios({
-      method: 'POST',
-      url: 'http://143.110.255.209:8000/search',
+      method: "POST",
+      url: "http://143.110.255.209:8000/search",
       headers: {
         "Content-Type": "application/json",
       },
-      data: searchInput
-    }).then((res) => {
-      setResult(res.data)
-    }).catch((e) => {
-      console.log({ error: e })
+      data: searchInput,
     })
-  }
-
+      .then((res) => {
+        setResult(res.data);
+      })
+      .catch((e) => {
+        console.log({ error: e });
+      });
+  };
 
   return (
     <div className="search-wrapper">
       <div className="searchbox-web">
         <div className="search-window">
-
           <div className="search-icon">
             <i className="fas fa-search fa-lg" id="icon"></i>
           </div>
 
-          <input className="search-area" type="text" value={input}
+          <input
+            className="search-area"
+            type="text"
+            value={input}
             onChange={(e) => {
-              setInput(e.target.value)
-              handleSearch()
+              setInput(e.target.value);
+              handleSearch();
             }}
             onFocus={() => {
-              setToggledrop(true)
+              setToggledrop(true);
             }}
             onBlur={() => {
-            setToggledrop(false)
+              setTimeout(() => {
+                setToggledrop(false);
+              }, 500);
             }}
           />
-
         </div>
       </div>
-      {result && toggledrop && <div className='search-results'> {result.map((track) => (
-        <div className='each-search-result' key={track._id}>
-          <div>
-            <img src={track.imageURL} className="search-result-image" />
-          </div>
-          <div className="search-result-title">
-            {track.title}
-          </div>
-          <div className="search-result-artist">
-            {track.artists[0]}
-          </div>
+      {result && toggledrop && (
+        <div className="search-results">
+          {" "}
+          {result.map((track) => (
+            <div
+              className="each-search-result"
+              key={track._id}
+              onClick={(e) => setPlayingTrack(track)}
+            >
+              <div>
+                <img src={track.imageURL} className="search-result-image" />
+              </div>
+              <div className="search-result-title">{track.title}</div>
+              <div className="search-result-artist">{track.artists[0]}</div>
+            </div>
+          ))}
         </div>
-      ))}
-      </div>}
+      )}
     </div>
-
   );
-}
+};
 
 export default Searchbox;
