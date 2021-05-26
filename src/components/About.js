b5fe4d1a0d0c78116fc83481de5828c49248f936
pranayvarshney <pranay.varshney9@gmail.com>
2021-05-26 21:36:14 +0530

    added under construction

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import bg from './158667.gif'
+import "./about.css"
+
+
+const About = () => {
+
+    return (<div className="test">
+        
+    </div>  );
+}
+ 
+export default About;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,35 +14,35 @@ const Sidebar = () => {
   };
   window.addEventListener("resize", showMobile);
   return (
-
-      <div className={mobile ? "sidebar-mobile" : "sidebar-web"}>
-        <div className="branding">
-          <div className="logo">
-            <img className="im" src={logo} height="80px" width="80px"  alt=""/>
-          </div>
-          <p align="left" className="brand-name">Synthesize</p>
+    <div className={mobile ? "sidebar-mobile" : "sidebar-web"}>
+      <div className="branding">
+        <div className="logo">
+          <img className="im" src={logo} height="80px" width="80px" alt="" />
         </div>
-        
-        <ul className="container sidebar-mainlist">
-          <li className="sidebar-list-items">
-            <Link to="/home">Home</Link>
-          </li>
-          {/* <li className="sidebar-list-items">
+        <p align="left" className="brand-name">
+          Synthesize
+        </p>
+      </div>
+
+      <ul className="container sidebar-mainlist">
+        <li className="sidebar-list-items">
+          <Link to="/home">Home</Link>
+        </li>
+        {/* <li className="sidebar-list-items">
             <Link to="/">Discover</Link>
           </li> */}
 
-          <li className="sidebar-list-items">
-            <Link to="">Playlists</Link>
-          </li>
-          <li className="sidebar-list-items">
-            <Link to="/">New Playlist</Link>
-          </li>
-          <li className="sidebar-list-items">
-            <Link to="/">Liked songs</Link>
-          </li>
-        </ul>
-      </div>
-
+        <li className="sidebar-list-items">
+          <Link to="/under_construction">Playlists</Link>
+        </li>
+        <li className="sidebar-list-items">
+          <Link to="/under_construction">New Playlist</Link>
+        </li>
+        <li className="sidebar-list-items">
+          <Link to="/under_construction">Liked songs</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
